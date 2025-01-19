2aef14e3c3a2cf50468cc9834b4a9c8c6e29968e
saikat-dev <eshikat355@rku.ac.in>
2025-01-20 00:58:34 +0600

    investment

--- database/migrations/2025_01_19_171212_create_invests_table.php
+++ database/migrations/2025_01_19_171212_create_invests_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('invests', function (Blueprint $table) {
             $table->id();
             $table->string('sector');
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('amount');
             $table->timestamps();
         });
